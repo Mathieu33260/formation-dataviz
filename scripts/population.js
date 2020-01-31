e9b524e480d4c547534867367a6f8bb3550e315c
Mathieu33260 <mat_dumez@hotmail.fr>
2020-01-31 11:00:45 +0100

    population

--- scripts/population.js
+++ scripts/population.js
@@ -1,7 +1,33 @@
-console.log('Good luck space cowboy !');
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-fetch(
-  'https://raw.githubusercontent.com/iOiurson/formation-dataviz/master/data/population.json',
-)
-  .then(resp => resp.json())
-  .then(data => console.log('DATA', data));
+const request = async () => {
+    const response = await fetch('../data/population.json');
+    const data = await response.json();
+    console.log(data);
+    let sum = data.map(city => city.population).reduce((acc, current) => acc + current);
+    console.log(sum);
+    const bar = document.getElementById('bar');
+    bar.style.height = '100px';
+    bar.style.width = '1000px';
+    data.forEach(city => {
+        let percent = city.population / sum * 100;
+        let color = getRandomColor();
+        console.log(city.name + ' ' + percent + ' ' + color);
+        const cityBar = document.createElement('div');
+        cityBar.style.height = '100px';
+        cityBar.style.width = percent * 10 + 'px';
+        cityBar.style.backgroundColor = color;
+        cityBar.textContent = city.name;
+        bar.append(cityBar);
+    })
+}
+
+
+request();
